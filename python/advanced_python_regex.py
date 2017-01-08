d27a1e07ea71c58ef3091a031c3fb7396ef3108a
immanencenow <ivan.khilko@gmail.com>
2017-01-08 03:58:43 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,10 +1,6 @@
 import pandas as pd
-import numpy as np
 faculty = pd.read_csv('faculty.csv')
 faculty['degree'] = faculty['degree'].str.replace(".","")
-#faculty1 = pd.concat([pd.Series(row['degree'], row['name'].split(' '))              
-                  # for _, row in faculty.iterrows()]).reset_index()
-#faculty1
 faculty2 = faculty.groupby(['degree']).size()
 faculty2 = faculty2.to_frame()
 faculty2 = faculty2.rename(columns={0: "freq"})
@@ -14,5 +10,7 @@ faculty2 = pd.concat([pd.Series(row['freq'], row['degree'].split(' '))
 faculty2 = faculty2.rename(columns={0: "freq"})
 faculty2 = faculty2.rename(columns={'index': "degree"})
 faculty2 = faculty2.groupby(['degree']).sum()
+faculty2 = faculty2.reset_index()
+faculty2.loc[faculty2.degree=='0', 'degree'] = 'No degree'
+faculty2 = faculty2.sort_values(by = 'freq', ascending=0)
 faculty2
-
