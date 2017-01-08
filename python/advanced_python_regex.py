46bf0b9d6f1f4c63dfe0fed64427dd3a1490b8a6
immanencenow <ivan.khilko@gmail.com>
2017-01-08 04:18:58 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -14,3 +14,16 @@ faculty2 = faculty2.reset_index()
 faculty2.loc[faculty2.degree=='0', 'degree'] = 'No degree'
 faculty2 = faculty2.sort_values(by = 'freq', ascending=0)
 faculty2
+
+import pandas as pd
+faculty = pd.read_csv('faculty.csv')
+faculty['title'] = faculty['title'].apply(lambda x: x.split(' of')[0])
+faculty['title'] = faculty['title'].apply(lambda x: x.split(' is')[0])
+faculty = faculty.groupby(['title']).size()
+faculty
+
+import pandas as pd
+faculty = pd.read_csv('faculty.csv')
+emails = faculty['email'].tolist()
+emails
+
